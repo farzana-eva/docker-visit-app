a193a0ffc11e60034fc473844bc6f7e2e91e3517
Farzana Eva <farzanaeva@Farzanas-iMac.local>
2023-02-11 23:47:44 -0500

    modified index.js file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const redis = require("redis");
+const process = require("process");
 
 const app = express();
 const client = redis.createClient({
@@ -9,11 +10,12 @@ const client = redis.createClient({
 client.set("visits", 0);
 
 app.get("/", (req, res) => {
+  process.exit(0);
   client.get("visits", (err, visits) => {
     res.send("Number of visits is " + visits);
     client.set("visits", parseInt(visits) + 1);
   });
 });
 app.listen(8081, () => {
-  console.log("Listening on port 8081");
+  console.log("Listening on port 4001");
 });
